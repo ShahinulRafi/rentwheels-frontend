6c7d8bbf5979ad048cb3107589d52a075d5a5154
ShahinulRafi <shahinul.csecu@gmail.com>
2026-01-07 18:43:31 +0600

    my bookings

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -9,7 +9,9 @@ const MyBookings = () => {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/myBookings?email=${user?.email}`)
+    if(!user?.email) return;
+
+    fetch(`http://localhost:5000/bookings?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => setMyBookings(data))
       .catch((err) => console.error(err));
@@ -58,27 +60,13 @@ const MyBookings = () => {
                 </th>
                 <td>
                   <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src={car?.imageURL}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
+              
                     <div>
-                      <div className="font-bold">{car?.displayName}</div>
-                      <div className="text-sm opacity-50">{car?.location}</div>
+                      <div className="font-bold">{car?.carName}</div>
                     </div>
                   </div>
                 </td>
-                <td>
-                  {car?.category}
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    {car?.description}
-                  </span>
-                </td>
+              
                 <td>{car?.price}</td>
                 <td className="flex gap-3">
                   <Link to={`/updateBookings/${car?._id}`}><button className="btn btn-primary btn-xs">Edit</button></Link>
